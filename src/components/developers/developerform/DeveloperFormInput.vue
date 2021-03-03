f4a915f2591b52b8f1884f66a3244f6482b79d00
Damian Obajtek <damian.obajtek@gmail.com>
2021-03-03 13:27:39 +0100

    form input

---
+++ src/components/developers/developerform/DeveloperFormInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-control" :class="{ invalid: !firstName.isValid }">
+    <label :for="name">{{ name }}</label>
+    <input
+      v-if="!textarea"
+      :type="type"
+      :id="name"
+      v-model.trim="name.val"
+      @blur="clearValidity(name)"
+    />
+    <textarea
+      v-esle
+      :id="name"
+      :rows="rows"
+      v-model.trim="description.val"
+      @blur="clearValidity('description')"
+    ></textarea>
+    <p v-if="!name.isValid">{{ name.capitalize() }} must not be empty.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: null
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text'
+    },
+    textarea: {
+      type: Boolean,
+      required: false,
+      default: null
+    },
+    rows: {
+      type: Number,
+      required: false,
+      default: null
+    }
+  }
+};
+</script>
+
+<style></style>
